ac26872cd3536c50715f174f1e33cfd8df296af1
neha.patil <neha.patil@zeuslearning.com>
2020-01-28 20:44:41 +0530

    Movie form WIP

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -4,68 +4,78 @@ import Form from "./common/form";
 import { getGenres } from "../services/fakeGenreService";
 
 class MovieForm extends Form {
-  state = {
-    data: {
-      _id: "",
-      title: "",
-      genre: "",
-      numberInStock: 7,
-      dailyRentalRate: 3.5
-    },
-    errors: {}
-  };
-  schema = {
-    genre: Joi.string(),
-    title: Joi.string()
-      .required()
-      .label("Title"),
-    numberInStock: Joi.number()
-      .required()
-      .label("Number in stock"),
-    dailyRentalRate: Joi.number()
-      .required()
-      .label("Daily rental rate")
-  };
+    state = {
+        data: {
+            _id: "5b21ca3eeb7f6fbccd471815",
+            title: "Terminator",
+            genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+            numberInStock: 6,
+            dailyRentalRate: 2.5,
+            publishDate: "2018-01-03T19:04:28.809Z",
+            liked: true
+        },
+        errors: {}
+    };
+    schema = {
+        title: Joi.string()
+            .required()
+            .label("Title"),
+        numberInStock: Joi.number()
+            .required()
+            .label("Number in stock"),
+        dailyRentalRate: Joi.number()
+            .required()
+            .label("Daily rental rate")
+    };
 
-  doSubmit = () => {
-    //Call to server
-    console.log("submitted");
-    //Navigate to movies page
-    this.props.history.replace("/movies");
-  };
-  handleChangeSelect = ({ target }) => {
-    const data = { ...this.state.data };
-    data[target.name] = target.value;
-    this.setState({ data });
-  };
-  render() {
-    return (
-      <div>
-        <h1>MovieForm {this.props.match.params.movieId}</h1>
-        {this.renderInput("title", "Title")}
-        <label className="my-1 mr-2" htmlFor="genre">
-          Genre
-        </label>
-        <select
-          className="custom-select my-1 mr-sm-2"
-          id="genre"
-          name="genre"
-          value={this.state.data.genre}
-          onChange={this.handleChangeSelect}
-        >
-          <option>Choose...</option>
-          {getGenres().map(genre => (
-            <option value={genre._id} key={genre._id}>
-              {genre.name}
-            </option>
-          ))}
-        </select>
-        {this.renderInput("numberInStock", "Number in stock")}
-        {this.renderInput("dailyRentalRate", "Daily rental rate")}
-        {this.renderSubmit("Save")}
-      </div>
-    );
-  }
+    doSubmit = () => {
+        //Call to server
+        console.log("submitted");
+        //Navigate to movies page
+        this.props.history.replace("/movies");
+    };
+    handleChangeSelect = ({ target }) => {
+        const data = { ...this.state.data };
+        data.genre._id = target.value;
+        const genres = getGenres();
+        data.genre.name = genres.find(
+            element => element._id === data.genre._id
+        ).name;
+        this.setState({ data });
+    };
+    componentDidMount() {
+        const movieId = this.props.match.params.movieId;
+    }
+    render() {
+        return (
+            <div>
+                <h1>MovieForm {this.props.match.params.movieId}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput("title", "Title")}
+                    <label className="my-1 mr-2" htmlFor="genre">
+                        Genre
+                    </label>
+                    <select
+                        className="custom-select my-1 mr-sm-2"
+                        id="genre"
+                        name="genre"
+                        value={this.state.data.genre._id}
+                        onChange={this.handleChangeSelect}
+                    >
+                        <option>Choose...</option>
+                        {getGenres().map(genre => (
+                            <option value={genre._id} key={genre._id}>
+                                {genre.name}
+                            </option>
+                        ))}
+                    </select>
+                    {this.renderInput("numberInStock", "Number in stock")}
+                    {this.renderInput("dailyRentalRate", "Daily rental rate")}
+                    {this.renderSubmit("Save")}
+                </form>
+            </div>
+        );
+    }
 }
 
 export default MovieForm;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -4,50 +4,62 @@ import Like from "./common/like";
 import Table from "./common/table";
 
 class MoviesTable extends Component {
-  headerColumns = [
-    {
-      path: "title",
-      label: "Title",
-      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
-    },
-    { path: "genre.name", label: "Genre" },
-    { path: "numberInStock", label: "Stock" },
-    { path: "dailyRentalRate", label: "Rate" },
-    {
-      key: "like",
-      content: movie => (
-        <Like
-          liked={movie.liked}
-          onClick={() => {
-            this.props.handleLike(movie);
-          }}
-        />
-      )
-    },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          className="btn btn-danger"
-          onClick={() => {
-            this.props.handleDelete(movie);
-          }}
-        >
-          Delete
-        </button>
-      )
+    headerColumns = [
+        {
+            path: "title",
+            label: "Title",
+            content: movie => (
+                <Link
+                    to={{
+                        pathname: "/tylermcginnis",
+                        state: {
+                            fromNotifications: true
+                        }
+                    }}
+                    to={`/movies/${movie._id}`}
+                >
+                    {movie.title}
+                </Link>
+            )
+        },
+        { path: "genre.name", label: "Genre" },
+        { path: "numberInStock", label: "Stock" },
+        { path: "dailyRentalRate", label: "Rate" },
+        {
+            key: "like",
+            content: movie => (
+                <Like
+                    liked={movie.liked}
+                    onClick={() => {
+                        this.props.handleLike(movie);
+                    }}
+                />
+            )
+        },
+        {
+            key: "delete",
+            content: movie => (
+                <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                        this.props.handleDelete(movie);
+                    }}
+                >
+                    Delete
+                </button>
+            )
+        }
+    ];
+    render() {
+        return (
+            <Table
+                headerColumns={this.headerColumns}
+                onSort={this.props.onSort}
+                sortOrder={this.props.sortOrder}
+                data={this.props.filteredMovies}
+            />
+        );
     }
-  ];
-  render() {
-    return (
-      <Table
-        headerColumns={this.headerColumns}
-        onSort={this.props.onSort}
-        sortOrder={this.props.sortOrder}
-        data={this.props.filteredMovies}
-      />
-    );
-  }
 }
 
 export default MoviesTable;
